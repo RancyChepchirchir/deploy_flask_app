ecb64816ced3f5f35810a9c1b4bcaf7173d8b197
Rancy Chepchirchir <kosgeyrancy2@gmail.com>
2020-12-21 20:22:51 +0300

    commits

--- app.py
+++ app.py
@@ -37,5 +37,6 @@ def predict():
 
 
 if __name__ == "__main__":
-    port=int(os.environ.get('PORT',5000))
-    app.run(port=port,debug=True,use_reloader=False)
+    #port=int(os.environ.get('PORT',5000))
+    #app.run(port=port,debug=True,use_reloader=False)
+    app.run(debug=True)
